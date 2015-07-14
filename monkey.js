40382b565e42da4245301d95c0c27d15b65cc4e1
Laurynas <laurynas@ginkus.lt>
2015-07-15 00:44:29 +0300

    showing sidebar with main info

--- monkey.js
+++ monkey.js
@@ -1,6 +1,3 @@
-/**
- * Created by Aurimas on 2015-07-14.
- */
 angular.module('biteMonkeyApp', ['ngMap'])
     .controller('MapsController', function ($scope, $timeout) {
         var mapsVm = $scope;
@@ -61,50 +58,86 @@ angular.module('biteMonkeyApp', ['ngMap'])
         $scope.$on('mapInitialized', function (event, map) {
             map.setCenter(new google.maps.LatLng(56, 24));
 
-            for (var i=0; i<mapsVm.stations.list.length; i++) {
+            //google.maps.event.addDomListener(window, 'resize', function() {
+            //    map.setCenter(center);
+            //});
+
+            google.maps.event.addDomListener(window, "resize", function() {
+                var center = map.getCenter();
+                google.maps.event.trigger(map, "resize");
+                map.setCenter(center);
+            });
+
+
+            for (var i = 0; i < mapsVm.stations.list.length; i++) {
                 var store = mapsVm.stations.list[i];
-                store.position = new google.maps.LatLng(store.location.latitude,store.location.longtitude);
-                store.title = store.title;
+                store.position = new google.maps.LatLng(store.location.latitude, store.location.longtitude);
+                //store.title = store.title;
 
-                $scope.positions.push({lat:store.location.latitude, lng: store.location.longtitude});
+                $scope.positions.push({lat: store.location.latitude, lng: store.location.longtitude});
 
-                var marker = new google.maps.Marker(store);
+                //var marker = new google.maps.Marker(store);
 
-                google.maps.event.addListener(marker, 'click', function() {
-                    mapsVm.openDetails(store);
-                    console.log('marker clicked');
+                var marker = new google.maps.Marker({
+                    position: store.position,
+                    map: map,
+                    title: store.title,
+                    text: store.text
                 });
-                google.maps.event.addListener(map, 'click', function() {
-                    closeDetails();
-                });
+
+                attachEventToMarker(marker, map);
+
             }
 
-/*            $scope.markerClusterer = new MarkerClusterer(map, mapsVm.stations.list, {});*/
+            google.maps.event.addListener(map, 'click', function () {
+                closeDetails();
+            });
+
+
+            /*            $scope.markerClusterer = new MarkerClusterer(map, mapsVm.stations.list, {});*/
             $timeout(function () {
                 google.maps.event.trigger(map, 'resize');
             });
         });
 
-        $scope.showMarkers = function() {
-            for (var key in $scope.map.markers) {
-                $scope.map.markers[key].setMap($scope.map);
-            };
-        };
+        //$scope.showMarkers = function() {
+        //    for (var key in $scope.map.markers) {
+        //        $scope.map.markers[key].setMap($scope.map);
+        //    };
+        //};
 
         $scope.openDetails = function (marker) {
-            $("#menu-toggle").click(function (e) {
-                e.preventDefault();
-                $("#wrapper").toggleClass("toggled");
+            console.log(marker);
 
-                console.log(marker);
-            });
+            $("#wrapper").removeClass("toggled");
+
+            $("#station-info #station-title").text(marker.title);
+            $("#station-info #station-description").text(marker.text);
+
             mapsVm.detailsOpened = true;
             mapsVm.stationDetails = mapsVm.stations.list[0];
+
         }
 
-        function closeDetails(){
+        function closeDetails() {
+            $("#wrapper").addClass("toggled");
             mapsVm.detailsOpened = false;
         }
 
+        function attachEventToMarker(marker, map){
+
+            google.maps.event.addListener(marker, 'click', function () {
+                mapsVm.openDetails(marker);
+
+                // kas á centra varytu  || tik ið antro karto :(
+                var center = map.getCenter();
+                google.maps.event.trigger(map, "resize");
+                map.setCenter(center);
+
+                map.panTo(marker.getPosition());
+                map.setZoom(12);
+
+            });
+        }
 
     });
