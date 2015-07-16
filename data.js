fc30c3659312c215e50a4bbfa0892fc8302c75b4
Laurynas <laurynas@ginkus.lt>
2015-07-16 12:13:51 +0300

    setting data file

---
+++ data.js
@@ -0,0 +1,263 @@
+var dataJS = [
+    {
+        company: 'Bite',
+        name: 'VL036',
+        municipality : 'Vilnius',
+        address : 'Vaduvos str. 21, Vilniaus m., Vilniaus m. sav. ',
+        lesser : 'UAB "Vilkpedës bûstas"',
+        leaseStart : new Date('2009-06-02'),
+        leaseProlonged : new Date('2020-06-01'),
+        autoExtension : true,
+        noticePeriod : new Date('2020-05-01'),
+        terminationLesser : 'by mutual agreement, breach of contract',
+        compensationLesser : 'if no violation of the agreement, compensate relocation costs',
+        terminationTenant : 'the notice period 6 months',
+        compensationTenant : 'none',
+        leaseMonth : 275.14,
+        leaseYear : 3301.67,
+        indexation : false,
+        paymentMethod : 'monthly',
+        network : '3G',
+        contacts : [
+            {
+                note: 'Nuomotojo atstovas',
+                name: 'Maryte Kvietkute',
+                email: 'sexy_bunny69@one.lt',
+                phone: '862965775'
+            },
+            {
+                note: 'Nuomotojo atstovas',
+                name: 'Petras Kirdeikis',
+                email: 'sexy_bunny68@one.lt',
+                phone: ''
+            }
+        ],
+        notes : [
+            {
+                user: 'Jonas Jonaitis',
+                date: '2015-07-10 10:43',
+                message: 'Pakalbëjau, viskas ok!'
+            },
+            {
+                user: 'Jonas Jonaitis',
+                date: '2015-07-13 15:13',
+                message: 'Jau nebe ok. lala'
+            }
+        ],
+        location: {
+            latitude: 54.6957829,
+            longitude: 25.2941403
+        }
+    },
+    {
+        company: 'Bite',
+        name: 'VL078',
+        municipality : 'Vilnius',
+        address : 'Gedimino pr. 26, Vilniaus m., Vilniaus m. sav. ',
+        lesser : 'UAB "CAN CAN"',
+        leaseStart : new Date('2008-12-22'),
+        leaseProlonged : new Date('2017-01-01'),
+        autoExtension : true,
+        noticePeriod : new Date('2016-12-09'),
+        terminationLesser : 'by mutual agreement, breach of contract',
+        compensationLesser : 'if no violation of the agreement, compensate relocation costs',
+        terminationTenant : 'the notice period 3 months',
+        compensationTenant : 'none',
+        leaseMonth : 100.23,
+        leaseYear : 1203.47,
+        indexation : true,
+        paymentMethod : 'monthly',
+        network : '4G',
+        contacts : [
+            {
+                note: 'neaiðku',
+                name: 'Jonas Jonauskas',
+                email: 'sexy_bunny69@one.lt',
+                phone: '862965775'
+            }
+        ],
+        notes : [
+            {
+                user: 'Jonas Jonaitis',
+                date: '2015-07-11 10:43',
+                message: 'Pakalbëjau, viskas ok!'
+            }
+        ],
+        location: {
+            latitude: 54.688246,
+            longitude: 25.273275
+        }
+    },
+    {
+        company: 'TeleTower',
+        name: 'KED02',
+        municipality : 'Këdainiai',
+        address : 'Vilnius str. 10, Këdainiai',
+        lesser : 'UAB "Panevëþio sûris"',
+        leaseStart : new Date('2010-09-01'),
+        leaseProlonged : new Date('2022-01-01'),
+        autoExtension : true,
+        noticePeriod : new Date('2021-05-01'),
+        terminationLesser : 'by mutual agreement, breach of contract',
+        compensationLesser : 'if no violation of the agreement, compensate relocation costs',
+        terminationTenant : 'the notice period 6 months',
+        compensationTenant : 'none',
+        leaseMonth : 204.14,
+        leaseYear : 2500.67,
+        indexation : false,
+        paymentMethod : 'monthly',
+        network : '4G',
+        contacts : [
+            {
+                note: 'Administratorius',
+                name: 'Maryte Kvietkute',
+                email: 'sexy_bunny69@one.lt',
+                phone: '862965775'
+            }
+        ],
+        notes : [
+            {
+                user: 'Jonas Jonaitis',
+                date: '2015-07-10 10:43',
+                message: 'Pakalbëjau, viskas ok!'
+            },
+            {
+                user: 'Jonas Jonaitis',
+                date: '2015-07-13 15:13',
+                message: 'Jau nebe ok. lala'
+            }
+        ],
+        location: {
+            latitude: 55.286985,
+            longitude: 23.940067
+        }
+    },
+    {
+        company: 'TeleTower',
+        name: 'KL010',
+        municipality : 'Klaipëda',
+        address : 'Upelio str. 12, Klaipëda',
+        lesser : 'UAB "Uostas"',
+        leaseStart : new Date('2010-11-15'),
+        leaseProlonged : new Date('2018-12-01'),
+        autoExtension : true,
+        noticePeriod : new Date('2018-11-01'),
+        terminationLesser : 'by mutual agreement, breach of contract',
+        compensationLesser : 'if no violation of the agreement, compensate relocation costs',
+        terminationTenant : 'the notice period 6 months',
+        compensationTenant : 'none',
+        leaseMonth : 120.54,
+        leaseYear : 2046.61,
+        indexation : false,
+        paymentMethod : 'monthly',
+        network : '3G',
+        contacts : [
+            {
+                note: 'sesuo',
+                name: 'Maryte Kvietkute',
+                email: 'sexy_bunny69@one.lt',
+                phone: '862977775'
+            },
+            {
+                note: 'Nuomotojas',
+                name: 'Petras Kirdeikis',
+                email: 'sexy_bunny68@one.lt',
+                phone: ''
+            }
+        ],
+        notes : [
+            {
+                user: 'Antanas Jonaitis',
+                date: '2015-07-10 10:43',
+                message: 'Pakalbëjau, viskas ok!'
+            },
+            {
+                user: 'Jonas Petrikas',
+                date: '2015-07-13 15:13',
+                message: 'Jau nebe ok. lala'
+            }
+        ],
+        location: {
+            latitude: 55.707723,
+            longitude: 21.124134
+        }
+    },
+    {
+        company: 'Bite',
+        name: 'PN098',
+        municipality : 'Panevëþys',
+        address : 'Vilniaus str. 54, Panevëþio m., Panevëþio m. sav. ',
+        lesser : 'UAB "Statybos trestas"',
+        leaseStart : new Date('2009-10-02'),
+        leaseProlonged : new Date('2020-10-01'),
+        autoExtension : true,
+        noticePeriod : new Date('2020-09-01'),
+        terminationLesser : 'by mutual agreement, breach of contract',
+        compensationLesser : 'if no violation of the agreement, compensate relocation costs',
+        terminationTenant : 'the notice period 6 months',
+        compensationTenant : 'none',
+        leaseMonth : 175.14,
+        leaseYear : 2301.67,
+        indexation : false,
+        paymentMethod : 'yearly',
+        network : '4G',
+        contacts : [
+            {
+                note: 'Atstovas - administratorë',
+                name: 'Maryte Kvietkute',
+                email: 'sexy_bunny69@one.lt',
+                phone: '862965775'
+            }
+        ],
+        notes : [
+            {
+                user: 'Rokiðkis Radiðkevièius',
+                date: '2014-07-10 10:43',
+                message: 'nori daugiau'
+            }
+        ],
+        location: {
+            latitude: 55.725219,
+            longitude: 24.355005
+        }
+    },
+    {
+        company: 'Bite',
+        name: 'SL016',
+        municipality : 'Ðiauliai',
+        address : 'Vilniaus str. 10, Ðiauliø m., Ðiauliø m. sav. ',
+        lesser : 'UAB "Ðiaurës vilkas"',
+        leaseStart : new Date('2010-02-01'),
+        leaseProlonged : new Date('2019-04-01'),
+        autoExtension : true,
+        noticePeriod : new Date('2019-03-04'),
+        terminationLesser : 'by mutual agreement, breach of contract',
+        compensationLesser : 'if no violation of the agreement, compensate relocation costs',
+        terminationTenant : 'the notice period 6 months',
+        compensationTenant : 'none',
+        leaseMonth : 475.14,
+        leaseYear : 6301.67,
+        indexation : false,
+        paymentMethod : 'monthly',
+        network : '4G',
+        contacts : [
+            {
+                note: 'Direktorius',
+                name: 'Petras Kirdeikis',
+                email: 'sexy_bunny68@one.lt',
+                phone: ''
+            }
+        ],
+        notes : [
+            {
+                user: 'Zigmas Butkus',
+                date: '2015-08-10 10:43',
+                message: 'Pakalbëjau, viskas ok!'
+            }
+        ],
+        location: {
+            latitude: 55.953717,
+            longitude: 23.354359
+        }
+    }
+];
